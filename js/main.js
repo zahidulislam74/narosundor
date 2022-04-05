086ee5cca1263f2444702e74319411c2fe9aa093
zahidulislam74 <99556044+zahidulislam74@users.noreply.github.com>
2022-04-05 11:00:54 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+$(document).ready(function(){
+      $('.owl-carousel').owlCarousel({
+        items:5,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })  
+});
+
